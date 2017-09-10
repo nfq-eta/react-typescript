9d02fec3cbe5f73eec26ce1c9e6ad5a65c58b87f
Vytenis <projektas@gmail.com>
2017-09-11 00:02:21 +0300

    Remove comments

--- config/webpack.config.ts
+++ config/webpack.config.ts
@@ -21,7 +21,6 @@ const config: webpack.Configuration = {
         chunkFilename: "js/[name].[hash].chunk.js",
     },
 
-    // Enable sourcemaps for debugging webpack's output.
     devtool: "source-map",
 
     resolve: {
@@ -69,23 +68,11 @@ const config: webpack.Configuration = {
             ],
             minChunks: Infinity,
         }),
-        // new webpack.optimize.CommonsChunkPlugin('vendor', 'vendor.js'),
-        /*new webpack.optimize.CommonsChunkPlugin({
-            names: ['vendors'],
-            filename: '[name].js'
-        }),*/
         new webpack.HotModuleReplacementPlugin(),
         new webpack.NamedModulesPlugin(),
     ],
 
-    // When importing a module whose path matches one of the following, just
-    // assume a corresponding global variable exists and use that instead.
-    // This is important because it allows us to avoid bundling all of our
-    // dependencies, which allows browsers to cache those libraries between builds.
-    externals: {
-        // "react": "React",
-        // "react-dom": "ReactDOM"
-    },
+    externals: {},
     devServer: {
         hot: true,
         // hotOnly: true,
