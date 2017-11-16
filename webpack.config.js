bbe374ef51a13bcba47d03005615435839019df2
Aivaras Biliunas <a.biliunas@openasapp.net>
2017-11-16 15:19:10 +0200

    Webpack issue

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 require("ts-node").register(
     {filename: "config/webpack.dev.ts"},
-    {filename: "config/webpack.prod.ts"},
+    {filename: "config/webpack.prod.ts"}
     );
 
 module.exports = env => require(`./config/webpack.${env}`).default;
