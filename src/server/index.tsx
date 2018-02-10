16eb0e23af24e65423ed0054361b86571c374d74
Vytenis <projektas@gmail.com>
2018-02-10 15:21:30 +0200

    fix(webpack): A bug fix
    Added performance optimizations to SSR
    
    fix #37

--- src/server/index.tsx
+++ src/server/index.tsx
@@ -1,4 +1,5 @@
 import * as path from 'path';
+import * as fs from 'fs';
 import * as express from 'express';
 import * as React from 'react';
 import HttpStatus from 'http-status-enum';
@@ -6,6 +7,7 @@ import { Provider } from 'react-redux';
 import { renderToString } from 'react-dom/server';
 import { StaticRouter } from 'react-router-dom';
 import * as getPort from 'get-port';
+import * as compression from 'compression';
 
 import { configureStore } from '../client/core/store';
 import { CoreRoutes } from '../client/core/routes';
@@ -16,24 +18,36 @@ const normalizePort = (val: number | string): number => {
     return typeof val === 'string' ? parseInt(val, 10) : val;
 };
 
+const readCss = (file: string) => {
+    const abs = path.resolve(file);
+    return fs.readFileSync(abs, 'utf8');
+};
+
+const styles = readCss('dist/css/app.css');
+const name = require('../../package.json').name;
+const description = require('../../package.json').description;
+
 const renderHtml = (html: string, preLoadedState: IRootState) => (
     `
     <!doctype html>
-    <html>
+    <html lang="en-us">
         <head>
             <meta charset="UTF-8">
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
             <meta http-equiv="X-UA-Compatible" content="ie=edge">
-            <title>Document</title>
-            <link rel="stylesheet" href="/css/app.css">
+            <title>${name}</title>
+            <meta name="Description" content="${description}">
+            <style>
+                ${styles}
+            </style>
         </head>
         <body>
             <div id="app">${html}</div>
             <script>
                 window.__PRELOADED_STATE__ = ${JSON.stringify(preLoadedState).replace(/</g, '\\u003c')}
             </script>
-            <script src="/js/vendors.js"></script>
-            <script src="/js/app.js"></script>
+            <script src="/js/vendors.js" async></script>
+            <script src="/js/app.js" async></script>
         </body>
     </html>
     `
@@ -41,6 +55,9 @@ const renderHtml = (html: string, preLoadedState: IRootState) => (
 
 const port = normalizePort(process.env.PORT || 8080);
 const app = express();
+
+// compress all requests
+app.use(compression());
 
 app.use('/js', express.static(path.join('dist', 'js'), { redirect: false }));
 app.use('/css', express.static(path.join('dist', 'css'), { redirect: false }));
