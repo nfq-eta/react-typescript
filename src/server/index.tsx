375a8ad514e0a5a4c034aa786d39d148e2cf7fb0
Vytenis <projektas@gmail.com>
2019-04-02 02:57:12 +0300

    ghdgfdgfdgfdhgf

--- src/server/index.tsx
+++ src/server/index.tsx
@@ -90,5 +90,7 @@ app.use((req: express.Request, res: express.Response) => {
 // getPort({ port }).then(rPort => {
 // tslint:disable-next-line:no-console
 // const PORT = process.env.PORT || 3000;
-app.listen(process.env.PORT || 3000);
+app.listen(parseInt(process.env.PORT), '0.0.0.0', function() {
+  console.log('Server started.......');
+});
 // });
